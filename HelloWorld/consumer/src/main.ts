efa5fcdf3b4fd545c6579e5f12a3777be31563e8
hannahellis4242 <hannahellis4242@gmail.com>
2023-04-15 14:21:31 +0100

    adding consumer

---
+++ HelloWorld/consumer/src/main.ts
@@ -0,0 +1,31 @@
+import { Channel, connect } from "amqplib";
+
+const host = process.env.QUEUE_HOST || "localhost";
+const url = `amqp://${host}`;
+const queue = "my-queue";
+
+const startup = async () => {
+  try {
+    const connection = await connect(url);
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queue, { durable: true });
+    channel.consume(queue, (data) => {
+      if (data) {
+        console.log(`${Buffer.from(data.content)}`);
+        channel.ack(data);
+      }
+    });
+    process.on("SIGINT", async () => {
+      try {
+        //await channel.close();
+        await connection.close();
+      } catch (e) {
+        console.error("On shutdown :", e);
+      }
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+startup();
