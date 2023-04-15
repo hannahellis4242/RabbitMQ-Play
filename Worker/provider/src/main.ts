12f8b6b0ffc678c75d51f8e177c3760564c7dd3f
hannahellis4242 <hannahellis4242@gmail.com>
2023-04-15 21:10:04 +0100

    Adding worker example

---
+++ Worker/provider/src/main.ts
@@ -0,0 +1,35 @@
+import { Channel, connect } from "amqplib";
+
+const host = process.env.QUEUE_HOST || "localhost";
+const url = `amqp://${host}`;
+const queue = "my-queue";
+
+const randomNumber = async (channel: Channel) => {
+  const i = Math.floor(Math.random() * 1000000);
+  const result = channel.sendToQueue(queue, Buffer.from(`${i}`));
+  if (!result) {
+    console.log("send to queue returned false");
+  }
+  setTimeout(() => randomNumber(channel), 200);
+};
+
+const startup = async (worker: (channel: Channel) => Promise<void>) => {
+  try {
+    const connection = await connect(url);
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queue, { durable: true });
+    await worker(channel);
+    process.on("SIGINT", async () => {
+      try {
+        //await channel.close();
+        await connection.close();
+      } catch (e) {
+        console.error("On shutdown :", e);
+      }
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+startup(randomNumber);
